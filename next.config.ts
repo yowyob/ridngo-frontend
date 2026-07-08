acc3952327e1c457606e4af15bc11de7c26bfd95
Savhel <jordantoulepi@gmail.com>
2026-07-08 20:40:36 +0100

    fix: next.config sans PWA (conflit turbopack/webpack)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: true,
-});
-
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
